f0f0aa5ff3004cf24570bb55ff943a1ba0bd8351
Sairam <satyasairamvankina@bitbucket.org>
2018-10-29 20:40:00 -0500

    created CRUD operations and login page view and rendered

--- yardstick/start/routes.js
+++ yardstick/start/routes.js
@@ -16,7 +16,4 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-// Route.on('/').render('welcome')
-Route.on('/').render('login')
-
-Route.resource('/jobs', 'JobController')
+Route.on('/').render('welcome')
