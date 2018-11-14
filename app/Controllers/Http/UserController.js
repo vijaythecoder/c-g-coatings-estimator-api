6953ff6d272e75c7fe33699078c9ac3a516f8b63
Sairam <satyasairamvankina@bitbucket.org>
2018-11-14 09:23:47 -0600

    authentication working model #11

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,33 +4,20 @@ class UserController {
     async store ({ request, response }) {
         const User = use('App/Models/User')
         const user = new User()
-     user.username = "asdfas"
+        user.username = "asdfas"
         user.email = 'virk@gmail.com'
         user.password = 'some-password'
         await user.save()
         console.log("Inside userController Store")
-        // User = {
-        //     email:"s531508@nwmissouri.edu",
-        //     password:"sairam123"
-        // };       
-        // User.save()
     }
 
     async login ({ request, auth }) {
 
         const { email, password } = request.all()
-        // const email = request.email
-        // const password = request.password
-        // console.log(email)
-        // console.log(password)
         console.log('Inside login ')
         console.log(auth.email)
         console.log(auth.id)
-        await auth.attempt(email, password)
-
-        // const user = await User.find(1)
-        // await auth.login(user)
-       
+        await auth.attempt(email, password)       
         return 'Logged in successfully'
     }
 
