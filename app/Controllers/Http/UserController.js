c3c643f1b0c05c76284d39e3a5cdd4adc4189ccd
Sairam <satyasairamvankina@bitbucket.org>
2018-11-18 18:49:47 -0600

    created CRUD operation views, rendered, provided routes for these views

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -4,20 +4,22 @@ class UserController {
     async store ({ request, response }) {
         const User = use('App/Models/User')
         const user = new User()
-        user.username = "asdfas"
-        user.email = 'virk@gmail.com'
-        user.password = 'some-password'
+        user.username = "sID"
+        user.email = 's531508@gmail.com'
+        user.password = 'asd'
         await user.save()
         console.log("Inside userController Store")
     }
 
     async login ({ request, auth }) {
-
+        await auth.logout()
         const { email, password } = request.all()
         console.log('Inside login ')
-        console.log(auth.email)
-        console.log(auth.id)
-        await auth.attempt(email, password)       
+
+        await auth.attempt(email, password)   
+        // console.log(auth.email)
+        // console.log(auth.id)
+        
         return 'Logged in successfully'
     }
 
@@ -26,6 +28,7 @@ class UserController {
         if (auth.user.id !== Number(params.id)) {
           return 'You cannot see someone else\'s profile'
         }
+        console.log(auth.user.username)
         return auth.user
       }
       
