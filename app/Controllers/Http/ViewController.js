3df17e8fffebf84b449943be47ca2f97249017c2
sai charan <S531499@nwmissouri.edu>
2019-01-25 19:04:03 -0600

    updated the viewcontroller with CRUD views routes

--- app/Controllers/Http/ViewController.js
+++ app/Controllers/Http/ViewController.js
@@ -4,6 +4,21 @@ class ViewController {
     async contact( {view} ){
         return view.render('contact')
     }
+    async create( {view} ){
+        return view.render('create')
+    }
+    async delete( {view} ){
+        return view.render('delete')
+    }
+    async edit( {view} ){
+        return view.render('edit')
+    }
+    async show( {view} ){
+        return view.render('show')
+    }
+    async estimate( {view} ){
+        return view.render('estimate')
+    }
 }
 
 module.exports = ViewController;
