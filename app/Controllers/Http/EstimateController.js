a390bdc7a2e0aadfe1ecb0dba31aa1a0517e3514
Sairam <satyasairamvankina@bitbucket.org>
2019-02-08 22:25:22 -0600

    updated CRUd operations on materials created materials.edge

--- app/Controllers/Http/EstimateController.js
+++ app/Controllers/Http/EstimateController.js
@@ -56,6 +56,7 @@ class EstimateController {
   async store ({ request, response, session }) {
     // Add logic here for saving the estimate
       const estimate = new Estimate()
+      const material = new Material()
       estimate.job_name = request.input('job_name')
       estimate.location = request.input('location')
       estimate.num_of_sqft = request.input('num_of_sqft')
@@ -70,7 +71,13 @@ class EstimateController {
       estimate.dollars_per_mile = request.input('dollars_per_mile')
       estimate.multiplier = request.input('multiplier')
       
+      material.product = request.input('product')
+      material.unit_cost = request.input('unit_cost')
+      material.coverage_area = request.input('coverage_area')
+      
       await estimate.save();
+      await material.save();
+
       session.flash({ notification: 'Estimate added!' })
       return response.redirect('/estimates')
   }
@@ -105,7 +112,8 @@ class EstimateController {
     
      console.log('edit');
      const estimate = await Estimate.find(params.id)
-     return view.render('estimates.edit', { estimate: estimate.toJSON()})
+     const material = await Material.find(params.id)
+     return view.render('estimates.edit', { estimate: estimate.toJSON(),material: material.toJSON()})
    
 
  
@@ -120,6 +128,7 @@ class EstimateController {
    */
   async update ({ params, request, response, session }) {
     const estimate = await Estimate.find(params.id)
+    const material = await Material.find(params.id)
     estimate.job_name = request.input('job_name')
    estimate.location = request.input('location')
    estimate.num_of_sqft = request.input('num_of_sqft')
@@ -134,8 +143,14 @@ class EstimateController {
    estimate.dollars_per_mile = request.input('dollars_per_mile')
    estimate.multiplier = request.input('multiplier')
    
+
+   material.product = request.input('product')
+   material.unit_cost = request.input('unit_cost')
+   material.coverage_area = request.input('coverage_area')
+   
    await estimate.save();
-   session.flash({ notification: 'Estimate updated!' })
+   await material.save();
+   session.flash({ notification: 'Estimate updated! Materials updated' })
    return response.redirect('/estimates')
     // update estimate
   }
@@ -150,8 +165,10 @@ class EstimateController {
    */
   async destroy ({ params, request, response, session }) {
     const estimate = await Estimate.find(params.id)
+    const material = await Material.find(params.id)
     await estimate.delete()
-    session.flash({ notification: 'Estimate Deleted!' })
+    await material.delete()
+    session.flash({ notification: 'Estimate and Material Deleted!' })
     return response.redirect('/estimates')  }
 }
 
