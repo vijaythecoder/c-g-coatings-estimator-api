15dc2970bea27a51098e2d402e95f0ed702f60bb
Devineni,Sai Sirisha <S531367@nwmissouri.edu>
2019-02-08 20:02:53 -0600

    rerun

--- database/migrations/1541168737135_estimates_schema.js
+++ database/migrations/1541168737135_estimates_schema.js
@@ -20,7 +20,8 @@ class EstimatesSchema extends Schema {
       table.integer('num_of_miles_pervehicle')
       table.float('dollars_per_mile')
       table.float('multiplier')
-    
+      table.integer('cost_id').references('id').inTable('MiscellaneousCosts')
+      table.integer('material_id').references('id').inTable('Materials')
       table.timestamps()
     })
   }
