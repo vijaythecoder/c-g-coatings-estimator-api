4c418a9191de18b0d8c658813156259f91ce4344
Sairam <satyasairamvankina@bitbucket.org>
2019-02-15 23:57:27 -0600

    seeder files for EstimatesMisccostSeeder.js and mislaneous cost

---
+++ database/seeds/EstimatesMisccostSeeder.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| EstimatesMisccostSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class EstimatesMisccostSeeder {
+  async run () {
+  }
+}
+
+module.exports = EstimatesMisccostSeeder
